362db0cefda9a46481b6c6d6db588f6e95fdf11c
Радик <a-radik-87@mail.ru>
2022-03-08 12:53:51 +0300

    Исправляет ошибки в коде функций

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
 const getRandomInteger = (min, max) => {
-  let rand = min + Math.random() * (max + 1 - min);
+  const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 };
-console.log(getRandomInteger(1, 4));
+getRandomInteger(0, 6);
 
-const getMaxLine = (testLine, maxLength) => {
-  if (testLine.length <= maxLength) {
-    return true;
-  } else {
-    return false;
-  }
-};
-console.log(getMaxLine('Hello world!!!', 44));
+const checkStringLength = (testLine, maxLength) => testLine.length <= maxLength;
+checkStringLength('Вау, классная фотка! Это где?', 100);
