2e56edc4ea5e49353e91df4d0a48ce54d3132e13
Радик <a-radik-87@mail.ru>
2022-04-13 15:07:29 +0300

    Исправялет ошибку в коде

--- js/miniatures.js
+++ js/miniatures.js
@@ -1,16 +1,19 @@
 const createUsersPhotos = (descriptionPictures) => {
   const picturesContainer = document.querySelector('.pictures');
   const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-  const pictureFragment = document.createDocumentFragment();
+
+  const picturesFragment = document.createDocumentFragment();
+
+
   descriptionPictures.forEach(({url, comments, likes}) => {
     const pictureElement = pictureTemplate.cloneNode(true);
     pictureElement.querySelector('.picture__img').src = url;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureFragment.appendChild(pictureElement);
-    picturesContainer.appendChild(pictureFragment);
+    pictureElement.querySelector('.picture__comments').textContent = likes;
+    pictureElement.querySelector('.picture__likes').textContent = comments.length;
+    picturesFragment.appendChild(pictureElement);
   });
+  picturesContainer.appendChild(picturesFragment);
 };
 
-export {createUsersPhotos};
 
+export {createUsersPhotos};
